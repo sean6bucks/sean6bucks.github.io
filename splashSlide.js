dbcff4cb0bb0fdfc2fe89c318ad0f2878c7e622b
sean6bucks <sean6bucks@gmail.com>
2015-03-29 18:59:03 -0400

    JQuery update slide trans.
    Applied smoother transparency  fade in addition to sliding up. Makes
    everything look a little cleaner and not abrupt.

--- splashSlide.js
+++ splashSlide.js
@@ -1,10 +1,20 @@
-var main = function() {
-	$('.enter-img').click(function() {
-		$('#splash').animate({
-			top: "-100%"
-		}, 300);
-	});
+// var main = function() {
+// 	$('.enter-img').click(function() {
+// 		$('#splash').animate({
+// 			top: "-100%"
+// 		}, 300);
 
+var main = function() {
+
+	$('.enter-img').click(function() {
+
+		// $('#splash').fadeOut("1000").slideUp("slow");
+		$('#splash')
+    .animate({top: '-100%', opacity: 0}, 'slow', function() {
+        $('#splash').remove();
+    });
+
+	});	
 };
 
 $(document).ready(main);
